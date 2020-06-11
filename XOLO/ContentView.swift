46cf0ed5b6bf1176d89b2ea1cb1e4ef3bdfb04b3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-06-11 11:14:16 +0300

    Initial Commit

---
+++ XOLO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XOLO
+//
+//  Created by ramil on 11.06.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
